ff62cd6d2461eadb967e6339e6da8ae1cf048ac2
Antti Tanskanen <ajtanskanen@users.noreply.github.com>
2023-09-18 22:52:13 +0300

    getting ready for the next paper

---
+++ data/savedata.R
@@ -0,0 +1,9 @@
+library("readxl")
+file="Github/Syntyvyyden-tekij-t/data/backtesting_pred.xlsx"
+predictions_backtesting_2019 <- read_excel(file,sheet = "2019")
+predictions_backtesting_2020 <- read_excel(file,sheet = "2020")
+predictions_backtesting_2021 <- read_excel(file,sheet = "2021")
+predictions_backtesting_2022 <- read_excel(file,sheet = "2022")
+predictions_backtesting_2018 <- read_excel(file,sheet = "2018")
+predictions_backtesting_2017 <- read_excel(file,sheet = "2017")
+save.image("~/Github/Syntyvyyden-tekij-t/data/predictions_backtesting_2023.RData")
